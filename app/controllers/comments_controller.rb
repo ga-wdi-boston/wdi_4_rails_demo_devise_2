ca50db33c106d1b82a2c2537180fd6f637fee8e1
David Fisher <tibbon@gmail.com>
2014-10-14 10:49:00 -0400

    Added login and registration view partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /comments
   # GET /comments.json
