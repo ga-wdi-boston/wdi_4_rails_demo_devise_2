fcdb2e4f9862c754d36b448b7bd49fae26f76379
David Fisher <tibbon@gmail.com>
2014-10-14 10:37:11 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :comments
+  root 'comments#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
