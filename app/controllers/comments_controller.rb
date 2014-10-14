2fc8995b313c5db8af5c9d7d3bbec3b5e872da24
David Fisher <tibbon@gmail.com>
2014-10-14 10:55:47 -0400

    Reverted sign_out button to :delete in the config, and added logic to comments_controller about the current_user.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-
+    @comment.user = current_user
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
